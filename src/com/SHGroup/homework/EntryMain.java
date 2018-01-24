a3d8abc0a0fc45e7db4c502535aa5a3468646030
SHGroup <sunghun7511@naver.com>
2018-01-25 01:08:45 +0900

    initialize packages

---
+++ src/com/SHGroup/homework/EntryMain.java
@@ -0,0 +1,8 @@
+package com.SHGroup.homework;
+
+public class EntryMain {
+    public static void main(String[] args) {
+        HomeworkClass hc = new Homework_2_7();
+        hc.run();
+    }
+}
---
+++ src/com/SHGroup/homework/HomeworkClass.java
@@ -0,0 +1,5 @@
+package com.SHGroup.homework;
+
+public abstract class HomeworkClass {
+    public abstract void run();
+}
